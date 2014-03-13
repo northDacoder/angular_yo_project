09eae3e7f926eaf3c7256a773c170fe1cec26252
John Ryan <jcryan85@gmail.com>
2014-03-13 11:12:11 -0700

    updated posts.html directives

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -2,9 +2,11 @@
 'use strict';
 
 app.controller('PostsCtrl', function($scope, Post){
-	$scope.posts = Post.get();
-	$scope.post = {url: 'http://', title: ''};
-
+	Post.save($scope.post, function(ref){
+		$scope.posts[ref.name] = $scope.post;
+		$scope.post = {url: 'http://', title: ''};
+	});
+	
 	$scope.submitPost = function() {
 		Post.save($scope.post);
 		$scope.post = {url: 'http://', title: ''};
