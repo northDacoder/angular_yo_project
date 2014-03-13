172a44579f6adb69a0041d5687dc3c8c45596c16
John Ryan <jcryan85@gmail.com>
2014-03-13 09:56:33 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yoyoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
