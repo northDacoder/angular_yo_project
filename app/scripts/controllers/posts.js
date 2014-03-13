70b4eb7cf1f15a243917c5ceeb357ac64418a3cb
John Ryan <jcryan85@gmail.com>
2014-03-13 11:13:23 -0700

    updated deletePost function

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -6,13 +6,15 @@ app.controller('PostsCtrl', function($scope, Post){
 		$scope.posts[ref.name] = $scope.post;
 		$scope.post = {url: 'http://', title: ''};
 	});
-	
+
 	$scope.submitPost = function() {
 		Post.save($scope.post);
 		$scope.post = {url: 'http://', title: ''};
 	};
 
-	$scope.deletePost = function() {
-		$scope.posts.splice(index, 1);
+	$scope.deletePost = function(postId) {
+		Post.delete({id: postId}, function() {
+			delete $scope.posts[postId];
+		});
 	};
 });
