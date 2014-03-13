84940d2b61a53937496e24a80f9d56372f3bc867
John Ryan <jcryan85@gmail.com>
2014-03-13 10:46:41 -0700

    added submitPost method to posts.js controller

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -3,5 +3,10 @@
 
 app.controller('PostsCtrl', function($scope){
 	$scope.posts = [];
-	$scope.post = {url: 'http://, title: ''};
+	$scope.post = {url: 'http://', title: ''};
+
+	$scope.submitPost = function() {
+		$scope.posts.push($scope.post);
+		$scope.post = {url: 'http://', title: ''};
+	}
 });
