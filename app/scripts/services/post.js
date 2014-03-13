96871f1b570128e00e929379cdfffe8806ae4861
John Ryan <jcryan85@gmail.com>
2014-03-13 11:22:52 -0700

    added create, find, delete functions to Post

--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -7,4 +7,18 @@ app.factory('Post',
 		var ref = new Firebase('https://yoyo.firebaseio.com/:id.json' + 'posts');
 
 		var posts = $firebase(ref);
+
+		var Post = {
+			all: posts,
+			create: function(post) {
+				return posts.$add(post);
+			},
+			find: function(postId) {
+				return posts.$child(postId);
+			},
+			delete: function(postId) {
+				return posts.$remove(postId);
+			}
+		};
+		return Post;
 });
