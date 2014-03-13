43591301a65a36aa5b16d1f0908624b5d643d35e
John Ryan <jcryan85@gmail.com>
2014-03-13 13:50:29 -0700

    added login view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,10 @@ app.config(function ($routeProvider) {
         templateUrl: 'views/register.html',
         controller: 'AuthCtrl'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
